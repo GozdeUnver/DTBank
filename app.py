c62f4a250c9746ec65fecce5f73f3b2ee6ef1432
gozde <gozdeunveer@gmail.com>
2023-02-06 17:24:29 +0100

    added files

---
+++ app.py
@@ -0,0 +1,89 @@
+from flask import Flask, request, render_template, url_for
+from flask_mysqldb import MySQL
+import hashlib
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'group4'
+app.config['MYSQL_DB'] = 'dtbank'
+mysql = MySQL(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods = ['POST'])
+def login():
+    username = request.form['Username']
+    #hashed_pw = hashlib.sha256(request.form['Password'].encode()).hexdigest()
+    hashed_pw=request.form['Password']
+    type = request.form['Type']
+    con = mysql.connection
+    cur = con.cursor()
+    if type == 'Manager':
+        cur.execute('SELECT COUNT(1) FROM DatabaseManager DM WHERE DM.username = %s AND DM.password = %s', (username, hashed_pw))
+        rc = int(cur.fetchone()[0]) # return code
+        con.commit()
+        if rc: return render_template('manager.html', username=username)
+        return render_template('login_error.html')
+    elif type == 'User': 
+        cur.execute('SELECT COUNT(1) FROM User U WHERE U.username = %s AND U.password = %s', (username, hashed_pw))
+        rc = int(cur.fetchone()[0]) # return code
+        con.commit()
+        if rc: return render_template('user.html', username=username)
+        return render_template('login_error.html')
+    else: return render_template('login_error.html')
+
+
+@app.route('/adduser', methods = ['GET', 'POST'])
+def add_user():
+    # TODO: Add a check for whether the user is already registered or not
+    if request.method == 'GET':
+        return render_template('adduser.html')
+    else:
+        hashed_pw = hashlib.sha256(request.form['Password'].encode()).hexdigest()
+        params =  (request.form['Username'], hashed_pw, request.form['Institute'])
+        print(params)
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO User(username, password, institute) \
+            VALUES (%s, %s, %s)", params)
+        mysql.connection.commit()
+        return render_template('manager.html', added_user=True)
+
+@app.route('/deletedrugs', methods = ['GET', 'POST'])
+def delete_drug():
+    # TODO: Add a check for whether the user is already registered or not
+    if request.method == 'GET':
+        return render_template('deletedrugs.html')
+    elif request.method == 'POST':
+        drugbank_id = request.form['drugid']
+        cur = mysql.connection.cursor()
+        cur.execute("DELETE FROM Drug WHERE drugbank_id = %s", (drugbank_id,))
+        cur.execute("SELECT ROW_COUNT()")
+        rc = int(cur.fetchone()[0])
+        mysql.connection.commit()
+        if rc:
+            return render_template('deletedrugs.html', success=True)
+        else: return render_template('deletedrugs.html', success=False)
+
+@app.route('/drugs',methods=['GET'])
+def drugs():
+    return render_template('drugsOptions.html')
+
+@app.route('/drugs/<subpath>',methods=['GET','POST'])
+def drugsSubpaths(subpath):
+    if subpath=='viewDrugs':
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM Drug D,(SELECT drugbank_id,target_name FROM Bindings) T,DrugCausedSideEffect S \
+            WHERE D.drugbank_id=S.drugbank_id \
+            AND D.drugbank_id=T.drugbank_id")
+        table=cur.fetchall()
+
+        # Html daha olmadı
+        return render_template('viewDrugs.html',table=table)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ db.py
@@ -0,0 +1,137 @@
+import MySQLdb
+
+# To run the following script, you need to have an active MySQL server running locally. 
+ # Enter your password and database name for the last two parameters, respectively
+
+
+con = MySQLdb.connect('localhost', 'root', 'group4','dtbank')
+cur = con.cursor()
+# Following code creates the refined tables
+cur.execute("CREATE TABLE User( \
+    username VARCHAR(30), \
+    institute VARCHAR(100), \
+    password CHAR(64), \
+    PRIMARY KEY(username,institute))")
+
+cur.execute("CREATE TABLE DatabaseManager(\
+    username VARCHAR(30), \
+    password VARCHAR(64), \
+    PRIMARY KEY(username))")
+
+cur.execute("CREATE TABLE UniProt( \
+    uniprot_id CHAR(6), \
+    sequence TEXT, \
+    PRIMARY KEY(uniprot_id))")
+
+cur.execute("CREATE TABLE Drug( \
+    drugbank_id CHAR(7), \
+    name VARCHAR(30), \
+    description TEXT, \
+    smiles VARCHAR(200), \
+    PRIMARY KEY(drugbank_id), \
+    UNIQUE(name), \
+    UNIQUE(smiles))")
+
+
+cur.execute("CREATE TABLE Interacts ( \
+    interactor_id CHAR(7), \
+    interactee_id CHAR(7), \
+    PRIMARY KEY(interactor_id,interactee_id), \
+    FOREIGN KEY (interactor_id) REFERENCES Drug(drugbank_id) ON DELETE CASCADE ON UPDATE CASCADE, \
+    FOREIGN KEY (interactee_id) REFERENCES Drug(drugbank_id) ON DELETE CASCADE ON UPDATE CASCADE)")
+
+cur.execute("CREATE TABLE SideEffectName( \
+    umls_cui CHAR(8), \
+    name VARCHAR(30), \
+    PRIMARY KEY(umls_cui), \
+    UNIQUE(name))")
+
+cur.execute("CREATE TABLE DrugCausedSideEffect( \
+    umls_cui CHAR(8), \
+    drugbank_id CHAR(7), \
+    PRIMARY KEY(umls_cui,drugbank_id), \
+    FOREIGN KEY (drugbank_id) REFERENCES Drug(drugbank_id) ON DELETE CASCADE ON UPDATE CASCADE, \
+    FOREIGN KEY (umls_cui) REFERENCES SideEffectName(umls_cui) ON DELETE CASCADE ON UPDATE CASCADE)")
+
+
+cur.execute("CREATE TABLE Bindings( \
+    reaction_id INTEGER, \
+    measure VARCHAR(4), \
+    affinity_nM INTEGER, \
+    doi VARCHAR(50), \
+    drugbank_id CHAR(7), \
+    uniprot_id CHAR(6), \
+    target_name VARCHAR(100), \
+    institute VARCHAR(100), \
+    PRIMARY KEY(reaction_id), \
+    FOREIGN KEY (drugbank_id) REFERENCES Drug(drugbank_id) ON DELETE CASCADE ON UPDATE CASCADE, \
+    FOREIGN KEY (uniprot_id) REFERENCES UniProt(uniprot_id) ON DELETE CASCADE ON UPDATE CASCADE)")
+
+cur.execute("CREATE TABLE Contributors( \
+    reaction_id INTEGER, \
+    username VARCHAR(30), \
+    institute VARCHAR(100), \
+    PRIMARY KEY (reaction_id, username), \
+    FOREIGN KEY (reaction_id) REFERENCES Bindings(reaction_id) ON DELETE CASCADE ON UPDATE CASCADE, \
+    FOREIGN KEY (username, institute) REFERENCES User(username, institute) ON DELETE CASCADE ON UPDATE CASCADE)")
+
+cur.execute("CREATE TABLE Points( \
+    institute VARCHAR(100), \
+    score INTEGER, \
+    PRIMARY KEY (institute))")
+
+
+# To do 1: Add the triggers
+cur.execute(
+    "create trigger deletedrug after delete on Drug for each row \n " \
+    "begin \n" \
+    "delete from Interacts where interactee_id=OLD.drugbank_id; \n "\
+    "delete from Bindings B where B.drugbank_id=OLD.drugbank_id; \n" \
+    "delete from DrugCausedSideEffect S where S.drugbank_id=OLD.drugbank_id; \n "\
+    "end \n " )
+
+cur.execute("delimiter // \
+    create trigger deleteprotein after delete on UniProt for each row \
+    begin \
+    delete from Bindings B where B.uniprot_id=OLD.uniprot_id; \
+    end// \
+    delimiter ;")
+
+cur.execute("delimiter // \
+    create trigger addPoints2 after insert on Contributors for each row \
+    begin \
+    update Points P set P.score=P.score+2 where P.institute=NEW.institute; \
+    end// \
+    delimiter ;")
+
+cur.execute("delimiter // \
+    create trigger deletePoints2 after delete on Contributors for each row \
+    begin \
+    update Points P set P.score=P.score-2 where P.institute=OLD.institute; \
+    end// \
+    delimiter ;")
+
+cur.execute("delimiter // \
+    create trigger addPoints5 after insert on Bindings for each row \
+    begin \
+    update Points P set P.score=P.score+5 where P.institute=NEW.institute; \
+    end// \
+    delimiter ;")
+
+# To do 2: Enforce the constraint that the DatabaseManager table can have at most 5 entries
+cur.execute("delimiter // \
+    create trigger limitDatabaseManager after insert on DatabaseManager for each row \
+    begin \
+    if (select count(*) from DatabaseManager)>5 then begin \
+    delete from DatabaseManager D where D.username=NEW.username; \
+    end; \
+    end if; \
+    end// \
+    delimiter ;")
+
+
+# TODO: Trigger to add institute after adding new user
+cur.execute("create trigger insertPoint after insert on User for each row \n " \
+    "begin \n" \
+    "insert into Points values (NEW.institute,0); \n "\
+    "end \n ")
